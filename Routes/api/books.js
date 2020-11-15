58894e693bac40c0cb52171cbe6743e4c8909307
ismahan jamea <ismahanjamea@gmail.com>
2020-11-14 22:28:35 -0800

    server

--- Routes/api/books.js
+++ Routes/api/books.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const booksController = require("../../booksController");
 
 // Matches with "/api/books"
 router.route("/")
